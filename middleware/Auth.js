4e01e13b4467716a04faf500eb22f3d141c6b601
Asfiya hussain <126233785+asfiyahussain00@users.noreply.github.com>
2025-08-23 12:11:40 +0500

    Update Auth.js

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 
 export default function auth(req, res, next) {
-  // Expecting: Authorization: Bearer <token>
   const header = req.headers.authorization || "";
   const token = header.startsWith("Bearer ") ? header.slice(7) : null;
 
@@ -9,7 +8,7 @@ export default function auth(req, res, next) {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.userId; // add to request
+    req.userId = decoded.userId;
     next();
   } catch (err) {
     return res.status(401).json({ error: "Invalid or expired token" });
